7834d05d23e6f6a18c755012c42f9e4185d74fe7
Eduard Loktev <87845853+Eduard-L@users.noreply.github.com>
2023-03-29 00:29:22 +0300

    add: q&a

--- src/components/Pages/App.jsx
+++ src/components/Pages/App.jsx
@@ -7,10 +7,13 @@ import { DataContext } from "../../context/DataContext";
 import { Loader } from "../features/Loader";
 import { PopupWithMessage } from "../features/PopupWithMessage";
 import { translationsObj } from "../../helpers/translations";
+import { Button } from "@mui/material";
+import DialogQuestionsAndAnswers from "../features/DialogQuestionsAndAnswers";
 
 function App() {
   const [step, setStep] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [openQA, setOpenQA] = useState(false);
   const [data, setData] = useState({
     isEnglish: false,
     translation: translationsObj["he"],
@@ -57,11 +60,31 @@ function App() {
         className="h-full flex flex-col justify-between items-center relative rounded-xl p-10  main-container"
         style={{ boxShadow: "0px 3px 20px #00000029" }}
       >
+        <DialogQuestionsAndAnswers
+          open={openQA}
+          handleClose={() => setOpenQA(false)}
+          isEnglish={data.isEnglish}
+          translation={data.translation}
+          direction={data.direction}
+        />
         <PopupWithMessage
           popupMessageProps={popupMessageProps}
           handleClose={handleClose}
         />
-
+        <Button
+          className=" rounded-xl bg-blue-500 absolute bottom-3 left-5"
+          variant="contained"
+          sx={{
+            width: 110,
+            transform: "translate(-10px, 0px)",
+            fontSize: "12px",
+          }}
+          onClick={() => {
+            setOpenQA(true);
+          }}
+        >
+          {data.translation.QandA}
+        </Button>
         <Logo />
         <DataContext.Provider
           value={{
---
+++ src/components/features/DialogQuestionsAndAnswers.jsx
@@ -0,0 +1,49 @@
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { QANDA_EN, QANDA_HE } from "../../helpers/translations";
+
+export default function DialogQuestionsAndAnswers({
+  open,
+  handleClose,
+  isEnglish,
+  direction,
+  translation,
+}) {
+  const questionsAndAnswersObject = isEnglish ? QANDA_EN : QANDA_HE;
+  return (
+    <div>
+      {/* <Button variant="outlined" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button> */}
+      <Dialog
+        open={open}
+        style={{ direction: direction }}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        className="flex flex-col justify-center items-center rounded-full"
+      >
+        <DialogTitle
+          className="text-center"
+          style={{ fontSize: "38px" }}
+          id="alert-dialog-title"
+        >
+          {translation?.QandA}
+        </DialogTitle>
+        <DialogContent>
+          {questionsAndAnswersObject.map((b, i) => {
+            return (
+              <div className={i !== 0 ? "mt-6" : " "} key={b.question}>
+                <h2 className="font-bold">
+                  {i + 1}. {b.question}
+                </h2>
+                <p className="mt-1">{b.answer}</p>
+              </div>
+            );
+          })}
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
